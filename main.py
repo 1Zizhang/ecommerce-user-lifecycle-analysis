8cd89952f0d52fbd46f260a45f1c34461738beeb
Zhang Haoxuan <mes_Zhanghaoxuan@outlook.com>
2026-08-19 12:10:48 +0800

    feat：重构代码函数，处理绘图异常情况

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
-from src.data_visualizer import init_plot_style
+from src.data_visualizer import init_plot_style, plot_monthly_sales
 from src.data_loader import load_ecommerce_data
 from src.data_processor import preprocess_data
-from src.data_visualizer import plot_monthly_sales
-file_path = 'data/ecommerce_transactions.txt'
+
+FILE_PATH = 'data/ecommerce_transactions.txt'
 
 
 def main():
-    # 初始化绘图风格
+    # 1.初始化绘图风格+中文字体显示
     init_plot_style()
-    # 日期转换
-    df =  preprocess_data(load_ecommerce_data(file_path))
-    # 画图
+    # 2.加载文件
+    raw_df = load_ecommerce_data(FILE_PATH)
+    # 3.字段格式转换
+    df = preprocess_data(raw_df)
+    # 4.画图
     plot_monthly_sales(df)
+
 
 if __name__ == '__main__':
     main()
--- src/data_visualizer.py
+++ src/data_visualizer.py
@@ -2,23 +2,50 @@ import matplotlib.pyplot as plt
 
 
 def init_plot_style(style_name: str = 'ggplot'):
-    """初始化绘图风格"""
+    """
+    初始化绘图风格
+    """
     plt.style.use(style_name)
     plt.rcParams['font.sans-serif'] = ['SimHei']
+    plt.rcParams['axes.unicode_minus'] = False
 
 
 def plot_monthly_sales(df):
-    '''
-    月产品销售数量图
+    """
+    月产品销售数量，消费金额，次数，消费人数图
     :param df: DataFrame
     :return: 无
-    '''
-    month_sale = df.groupby(by='month')['order_products'].sum()
-    fig, ax = plt.subplots(figsize=(10, 4))
-    month_sale.plot(ax=ax)
-    ax.set_title("每月商品总销量")
-    ax.set_xlabel("月份")
-    ax.set_ylabel("商品总销量")
-    plt.xticks(rotation=45)
+    """
+    fig, axes = plt.subplots(2, 2, figsize=(14, 10))
+
+    # 1.每月产品购买数量
+    ax = axes[0, 0]
+    df.groupby('month')['order_products'].sum().plot(ax=ax, kind='line', marker='o')
+    ax.set_title('月度产品购买总数量趋势')
+    ax.set_xlabel('月份')
+    ax.set_ylabel('购买总数量')
+
+    # 2.每月消费金额
+    ax = axes[0, 1]
+    df.groupby('month')['order_amount'].sum().plot(ax=ax, kind='line', marker='o')
+    ax.set_title('月度累计消费金额趋势')
+    ax.set_xlabel('月份')
+    ax.set_ylabel('消费总金额')
+
+    # 3.每月消费次数
+    ax = axes[1, 0]
+    df.groupby('month')['user_id'].count().plot(ax=ax, kind='line', marker='o')
+    ax.set_title('月度订单次数趋势')
+    ax.set_xlabel('月份')
+    ax.set_ylabel('订单次数')
+
+    # 4.每月消费人数
+    ax = axes[1, 1]
+    df.groupby('month')['user_id'].nunique().plot(ax=ax, kind='line', marker='o')
+    ax.set_title('月度活跃消费人数趋势')
+    ax.set_xlabel('月份')
+    ax.set_ylabel('独立消费人数')
+
     plt.tight_layout()
     plt.show()
+    plt.close()
